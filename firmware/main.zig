4c76543c8eb348aab647ef4d053466abf13200e3
vegecode <justin.b.alexander1@gmail.com>
2020-03-11 14:08:17 -0500

    Begin initialization using zig-cortex and svd2zig generated header

--- firmware/main.zig
+++ firmware/main.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const reg = @import("STM32F7x7.zig");
+const cpu = @import("zig-cortex/cm7.zig");
 
 comptime {
     // force a reference to file
@@ -6,5 +8,17 @@ comptime {
 }
 
 pub export fn main() noreturn {
+    initHardware();
     while (true) {}
+}
+
+fn initHardware() void {
+    cpu.SCB.ICache.enable();
+    cpu.SCB.DCache.enable();
+    // enable ART Accelerator and prefetch buffer
+    reg.Flash_ACR_Ptr.* |= reg.Flash_ACR_ARTEN_Mask | reg.Flash_ACR_PRFTEN_Mask;
+}
+
+test "Semantic Analyze" {
+    std.meta.refAllDecls(@This());
 }
