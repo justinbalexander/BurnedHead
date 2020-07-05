c15e511b11892430f12ba61645e8fc64bbf64bde
vegecode <justin.b.alexander1@gmail.com>
2020-07-05 10:56:34 -0500

    zig build uses release safe for debug (embedded friendly)

--- build.zig
+++ build.zig
@@ -4,7 +4,8 @@ const builtin = @import("builtin");
 
 pub fn build(b: *Builder) void {
     const exe = b.addExecutable("executable.elf", "firmware/main.zig");
-    exe.setBuildMode(b.standardReleaseOptions());
+    const mode = embeddedFriendlyReleaseOptions(b);
+    exe.setBuildMode(mode);
     exe.setLinkerScriptPath("firmware/linker.ld");
 
     exe.setTarget(
@@ -16,4 +17,22 @@ pub fn build(b: *Builder) void {
 
     b.default_step.dependOn(&exe.step);
     exe.install();
+}
+
+fn embeddedFriendlyReleaseOptions(self: *Builder) builtin.Mode {
+    if (self.release_mode) |mode| return mode;
+
+    const release_fast = self.option(bool, "release-fast", "Optimizations on and safety off") orelse false;
+    const release_small = self.option(bool, "release-small", "Size optimizations on and safety off") orelse false;
+
+    const mode = if (release_fast and !release_small)
+        builtin.Mode.ReleaseFast
+    else if (release_small and !release_fast)
+        builtin.Mode.ReleaseSmall
+    else
+        builtin.Mode.ReleaseSafe;
+
+    self.is_release = mode != .ReleaseSafe;
+    self.release_mode = mode;
+    return mode;
 }
